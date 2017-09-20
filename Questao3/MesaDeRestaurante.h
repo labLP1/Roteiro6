0ca37b61562e403d36df046471c8bef2452ac6e5
Julio <julio.rapha@hotmail.com>
2017-09-20 20:46:25 -0300

    Questao 3 ainda falta o main

---
+++ Questao3/MesaDeRestaurante.h
@@ -0,0 +1,28 @@
+/*
+ * MesaDeRestaurante.h
+ *
+ *  Created on: 20/09/2017
+ *      Author: Raphael
+ */
+
+#ifndef MESADERESTAURANTE_H_
+#define MESADERESTAURANTE_H_
+
+#include <iostream>
+#include "Pedido.h"
+#define MAX_PEDIDOS 100
+using namespace std;
+
+class MesaDeRestaurante {
+private:
+	Pedido pedido[MAX_PEDIDOS];
+	int auxiliar = 0;
+
+public:
+	void adiconaAoPedido();
+	void zeraPedidos();
+	double calculaPedidos();
+	MesaDeRestaurante();
+};
+
+#endif /* MESADERESTAURANTE_H_ */
